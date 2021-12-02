bc7131eaf81a48e99c9614b3d4a4b58598a1f5d6
Ivan <vipanda569@mail.ru>
2021-12-02 18:33:11 +0700

    Unit & Mockito test

--- src/test/java/spring_react/demo/ServiceTest/ServiceTest.java
+++ src/test/java/spring_react/demo/ServiceTest/ServiceTest.java
@@ -1,6 +1,5 @@
 package spring_react.demo.ServiceTest;
 
-
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,7 +20,6 @@ public class ServiceTest {
     @Autowired
     @MockBean
     EmployeeService employeeService;
-
 
     @MockBean
     EmployerRepository employerRepository;
